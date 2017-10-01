012d8cb7bd2e077e56b927c15b8644139ab12b11
ganxm <820185348@qq.com>
2017-10-01 21:52:07 +0800

    第一次提交

---
+++ html/myblog/js/main.js
@@ -0,0 +1,12 @@
+function ToArticleJsq(){
+	window.location.href="ArticleJsq.html";
+}
+
+function ToArticlecjp(){
+    window.location.href="Articlecjp.html";
+}
+
+function ToArticleajax(){
+    window.location.href="Article-ajax.html";
+}
+
