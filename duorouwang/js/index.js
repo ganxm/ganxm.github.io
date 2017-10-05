40fa58f9b320049bebba7f9b8c3747473b0f3b3a
ganxm <820185348@qq.com>
2017-10-05 20:21:53 +0800

    添加响应式布局

---
+++ duorouwang/js/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by 82018 on 2017/10/5.
+ */
+function pageScroll() {
+    window.scrollBy(0, -10000);
+    // scrolldelay = setTimeout('pageScroll()', 22);
+    // console.log(document.body.scrollTop);
+    // if (document.body.scrollTop == -80) {
+    //     console.log(document.body.scrollTopOffset);
+    //     clearTimeout(scrolldelay);
+    // }
+};
+
+//$(window).scroll(function() {
+//    if ($(window).scrollTop() > 200){
+//        $('#goto-top').show();
+//    }else{
+//        $('#goto-top').hide();
+//    }
+//});
